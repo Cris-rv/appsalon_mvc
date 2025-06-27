aecfc7f4bc8de6c6c048a0f94616ded699ed0f51
Cristopher <rivasbritoc@gmail.com>
2025-06-26 22:24:09 -0600

    Moviendo Router a includes para mejor orden

--- public/text.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "<pre>";
-print_r([
-    'SERVER_INFO' => $_SERVER['SERVER_SOFTWARE'] ?? 'N/A',
-    'REQUEST_METHOD' => $_SERVER['REQUEST_METHOD'] ?? 'N/A',
-    'ALL_SERVER_KEYS' => array_keys($_SERVER),
-    'PATH_INFO' => $_SERVER['PATH_INFO'] ?? 'N/A',
-    'ORIG_REQUEST_URI' => $_SERVER['HTTP_X_ORIGINAL_URI'] ?? 'N/A' // Usado en algunos proxies
-]);
