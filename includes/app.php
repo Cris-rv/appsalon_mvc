152f728986d1cdda3eb4c569218c8b3ca1d7abd5
Cristopher <rivasbritoc@gmail.com>
2025-06-28 13:02:16 -0600

    router

--- includes/app.php
+++ includes/app.php
@@ -8,7 +8,6 @@ $dotenv->safeLoad();
 
 require 'funciones.php';
 require 'database.php';
-debuguear(__DIR__ . '/../vendor/autoload.php');
 
 // Conectarnos a la base de datos
 ActiveRecord::setDB($db);
